3a98f6fd33b4409825523c6b2054cb4e01bffb15
Amandeep Saini <asaini@wha-mbp-as.local>
2020-06-26 19:49:19 -0700

    updated review projects

---
+++ 2-counter/setup/app.js
@@ -0,0 +1,21 @@
+let count = 0; 
+const value = document.querySelector("#value");
+
+const btns = document.querySelectorAll(".btn")
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')) {
+            count--;
+        }
+        else if(styles.contains('increase')) {
+           count++; 
+        }
+        else if (styles.contains('reset')) {
+            count = 0; 
+        }
+        (count > 0)? value.style.color = 'green': value.style.color = 'red';
+        
+        value.textContent = count
+      });
+});
